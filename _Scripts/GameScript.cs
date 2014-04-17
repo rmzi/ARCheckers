beb8e69145107b6ab1fd5117a0ba168423d98bb5
Ramzi Abdoch <ziggy@dyn-160-39-195-224.dyn.columbia.edu>
2014-04-16 21:21:56 -0400

    Got board working
    In the GameScript under the Board object, there is a start function.
    There, I instantiate each of the board pieces and change their color.
    They are all children of the Board object.

--- _Scripts/GameScript.cs
+++ _Scripts/GameScript.cs
@@ -8,19 +8,21 @@ public class GameScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Transform newPiece = (Transform)Instantiate (boardPiecePrefab, transform.position, Quaternion.identity);
-		Transform board = GameObject.FindGameObjectWithTag ("Board").transform;
-		newPiece.parent = board;
-		newPiece.renderer.material.color = Color.green;
+		int boardSize = 8;
 
-		int boardSize = 8
-		/*
 		for(int i = 0; i < boardSize; i++){
-			for(int i = 0; i < boardSize; i++){
-				Instantiate(boardPiecePrefab, 
+			for(int j = 0; j < boardSize; j++){
+				Vector3 nextLocation = new Vector3(-35 + 10f * i, 0, -35 + 10f * j);
+				Transform newPiece = (Transform)Instantiate(boardPiecePrefab, nextLocation, Quaternion.identity);
+				Transform board = GameObject.FindGameObjectWithTag ("Board").transform;
+				newPiece.parent = board;
+				if((i + j) % 2 == 0){
+					newPiece.renderer.material.color = Color.white;
+				} else {
+					newPiece.renderer.material.color = Color.black;
+				}
 			}
 		}
-		*/
 	}
 	
 	// Update is called once per frame
