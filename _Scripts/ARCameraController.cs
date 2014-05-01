e857fe0e34774a07c1bebd3164fdc5392acfaed2
annania <amm2324@columbia.edu>
2014-05-01 06:14:41 -0400

    instructions, moved gui

--- _Scripts/ARCameraController.cs
+++ _Scripts/ARCameraController.cs
@@ -35,6 +35,8 @@ public class ARCameraController : MonoBehaviour {
 	public bool p1;
 	public bool p2;
 	public GUIStyle turn;
+	public GUIStyle redP2;
+	public GUIStyle redP1;
 
 	//Screen dimensions
 	private int height = Screen.height;
@@ -54,6 +56,7 @@ public class ARCameraController : MonoBehaviour {
 	
 	// Selection
 	private GameObject selectedObject;
+	private bool selMode; //used for gui
 
 	//bools handling menu
 	private bool showMenu;
@@ -68,6 +71,9 @@ public class ARCameraController : MonoBehaviour {
 		sysinfo.text = "Approach a game piece to select it.";
 		p1 = false;
 		p2 = false;
+		highMode = PREGAME_MODE;
+		lowMode = INIT_MODE;
+		selMode = false; //used for gui
 	}
 
 
@@ -81,7 +87,7 @@ public class ARCameraController : MonoBehaviour {
 		// Debug
 		// Draw ray during pause mode with sphere at the end
 		Debug.DrawRay (transform.position, focusRay.direction * focusDistance);
-		debugSphere = GameObject.FindGameObjectWithTag ("debug").transform;
+		debugSphere = GameObject.FindGameObjectWithTag("debug").transform;
 		debugSphere.position = focusPoint;
 		if (highMode == PREGAME_MODE){
 			// DON'T DO ANYTHING!
@@ -123,6 +129,7 @@ public class ARCameraController : MonoBehaviour {
 		//if the target is detected before game starts it will show menu
 		DefaultTrackableEventHandler s = ImageTarget.GetComponent<DefaultTrackableEventHandler>();
 		menu = s.startGame;
+		//menu = true;
 		
 	}
 	
@@ -132,13 +139,18 @@ public class ARCameraController : MonoBehaviour {
 		///////////////
 		if (showStart) {
 			if(menu){
-				if (GUI.Button (new Rect (300, 110, 150, 100), "START")) {
+				//if (GUI.Button (new Rect (300, 110, 150, 100), "START")) {
+			if (GUI.Button (new Rect ((width/2), (height-100), 150, 100), "START")) {
 					showMenu = true;
 					showStart=false;
 					p1 = true;
 					GameScript b = Board.GetComponent<GameScript> ();
 					b.makeBoard ();
+					lowMode=SEL_MODE;
 				}
+				turn.normal.textColor = Color.magenta;
+				GUI.Label((new Rect (((width/2)-50),0, 150, 50)),"INSTRUCTIONS", turn);
+				GUI.Label((new Rect (((width/5)-50),50, 550, 300)),"Black moves first. Players then alternate moves.Moves are allowed only on\nthe dark squares, so pieces always move diagonally. Single pieces are\nalways limited to forward moves (toward the opponent). A piece making a non-capturing\nmove (not involving a jump) may move only one square. A piece making a capturing \nmove (a jump) leaps over one of the opponent's pieces, landing in a straight \ndiagonal line on the other side. Only one piece may be captured in a single jump; \nhowever, multiple jumps are allowed on a single turn.When a piece is captured, it is \nremoved from the board.If a player is able to make a capture, there is no option -- the \njump must be made. If more than one capture is available, the player is free \nto choose whichever he or she prefers.When a piece reaches the furthest row from the player \nwho controls that piece, it is crowned and becomes a king. One of the pieces which \nhad been captured is placed on top of the king so that it is twice as high as a single \npiece. Kings are limited to moving diagonally, but may move both forward and backward. \n(Remember that single pieces, i.e. non-kings, are always limited to forward moves.)\nKings may combine jumps in several directions -- forward and backward -- on the same turn. \nSingle pieces may shift direction diagonally during a multiple capture turn, but must \nalways jump forward (toward the opponent). A player wins the game when the opponent cannot make a move. \nIn most cases, this is because all of the opponent's pieces have been captured, but it \ncould also be because all of his pieces are blocked in.", turn);
 			}
 		}
 		//player message
@@ -162,33 +174,41 @@ public class ARCameraController : MonoBehaviour {
 
 			/// Pick Up
 			/// Place
-			if (GUI.Button (new Rect (350, (height-100), 150, 100), "Select Object")) {
-				lowMode = MOVE_MODE;
+			if (!selMode){
+				if (GUI.Button (new Rect ((width-250), (height-100), 150, 100), "Select Piece")) {
+					lowMode = MOVE_MODE;
+					selMode = true;
+
+				}
 			}
-	
 			// Deselection
-			if (GUI.Button (new Rect (200,(height-100), 150, 100), "Deselect Object")) {
-				lowMode = SEL_MODE;
+			if (selMode){
+				if (GUI.Button (new Rect ((width-250),(height-100), 150, 100), "Deselect Piece")) {
+					lowMode = SEL_MODE;
+					selMode = false;
+				}
 			}
 			/////////////////
     		 // Translation //
 			/////////////////
-			if (GUI.RepeatButton (new Rect (100, (height-100), 100, 100), "Explore")) {
+			if (GUI.RepeatButton (new Rect ((width-100), (height-100), 100, 100), "Explore")) {
 				 highMode = EXPLORE_MODE;
 			}
 			/////////////////
 			//   NewGame   //
 			/////////////////
-			if (GUI.RepeatButton (new Rect (0, (height-100), 100, 100), "New Game")) {
+			if (GUI.RepeatButton (new Rect ((width-350), (height-100), 100, 100), "New Game")) {
 				//translateMode = true;
 			}
 			int number = 8;
-			GUI.Label((new Rect (0, 0, 100, 100)),"Player 1");
-			GUI.Label((new Rect (0, 50, 100, 100)),"Count: "+ number);
-			GUI.Label((new Rect (0, 100, 100, 100)),"Score: "+ number);
-			GUI.Label((new Rect ((width-50), 0, 100, 100)),"Player 2");
-			GUI.Label((new Rect ((width-50), 50, 100, 100)),"Count: "+ number);
-			GUI.Label((new Rect ((width-50), 100, 100, 100)),"Score: "+ number);
+			redP2.normal.textColor = Color.red;
+			redP1.normal.textColor = Color.white;
+			GUI.Label((new Rect ((width-100), 0, 100, 100)),"Player 1", redP1);
+			GUI.Label((new Rect ((width-100), 50, 100, 100)),"Count: "+ number, redP1);
+			GUI.Label((new Rect ((width-100), 100, 100, 100)),"Score: "+ number, redP1);
+			GUI.Label((new Rect ((width-50), 0, 100, 100)),"Player 2", redP2);
+			GUI.Label((new Rect ((width-50), 50, 100, 100)),"Count: "+ number, redP2);
+			GUI.Label((new Rect ((width-50), 100, 100, 100)),"Score: "+ number, redP2);
 
 		}
 		
